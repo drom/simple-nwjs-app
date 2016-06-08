ddb92ad60f59424f020a57e0646b56d2ead8a913
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2016-06-08 20:52:45 +0300

    bumped NWJS to v0.15.2

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #!/usr/bin/sh
-wget -nc http://dl.nwjs.io/v0.13.0-rc2/nwjs-v0.13.0-rc2-linux-x64.tar.gz
-tar -xvsf nwjs-v0.13.0-rc2-linux-x64.tar.gz
+wget -nc http://dl.nwjs.io/v0.15.2/nwjs-v0.15.2-linux-x64.tar.gz
+tar -xvsf nwjs-v0.15.2-linux-x64.tar.gz
 cd src
-../nwjs-v0.13.0-rc2-linux-x64/nw .
+../nwjs-v0.15.2-linux-x64/nw .
