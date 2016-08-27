7b25ce881a6fca3dc4331943f24bf6f1e2cb62e7
Aliaksei Chapyzhenka <alex.drom@gmail.com>
2016-08-26 22:55:52 -0700

    removed redundant nw copy

--- build.sh
+++ build.sh
@@ -13,6 +13,7 @@ rm -rf build/*
 pushd src && zip -r ../build/$N-$V.nw * && popd
 
 mkdir cache
+rm -rf cache/*/*
 
 for P in linux-x64 linux-x86
 do
@@ -35,21 +36,23 @@ done
 
 for P in linux-x64 linux-x86
 do
-  cat cache/nwjs-$NWV-$P/nw build/$N-$V.nw > cache/$N-$V-$P/$N
+  cat cache/$N-$V-$P/nw build/$N-$V.nw > cache/$N-$V-$P/$N
   chmod +x cache/$N-$V-$P/$N
+  rm  cache/$N-$V-$P/nw
   pushd cache && tar -cvzf ../build/$N-$V-$P.tar.gz $N-$V-$P/* && popd
 done
 
 for P in win-x64 win-x86
 do
-  cat cache/nwjs-$NWV-$P/nw.exe build/$N-$V.nw > cache/$N-$V-$P/$N.exe
+  cat cache/$N-$V-$P/nw.exe build/$N-$V.nw > cache/$N-$V-$P/$N.exe
+  rm  cache/$N-$V-$P/nw.exe
   pushd cache && zip -r ../build/$N-$V-$P.zip $N-$V-$P/* && popd
 done
 
 for P in osx-x64
 do
-  cat cache/nwjs-$NWV-$P/nwjs.app/Contents/MacOS/nwjs \
+  cat cache/$N-$V-$P/nwjs.app/Contents/MacOS/nwjs \
   build/$N-$V.nw > cache/$N-$V-$P/nwjs.app/Contents/MacOS/$N
-
+  rm  cache/$N-$V-$P/nwjs.app/Contents/MacOS/nwjs
   pushd cache && zip -r ../build/$N-$V-$P.zip $N-$V-$P/* && popd
 done
